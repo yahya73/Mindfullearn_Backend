ae27d22399f4d4c25655d0d2dc0bd7981b12efe7
yahya73 <yahya.fhima@esprit.tn>
2024-01-02 02:44:57 +0100

    imageuser false

--- model/Post.js
+++ model/Post.js
@@ -42,7 +42,7 @@ const PostSchema = new mongoose.Schema(
     likes: [likeSchema],
     userImage: {
       type: String,
-      required: true,
+      required: false,
     },
     userId: {
       type: String,
